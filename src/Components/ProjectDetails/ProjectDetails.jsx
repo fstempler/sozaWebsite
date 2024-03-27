6e63593885b0c3958e1a79edeecf67e19dc3143b
fstempler <zackmango@gmail.com>
2024-03-27 16:21:03 -0300

    Fix Project Pages

--- src/Components/ProjectDetails/ProjectDetails.jsx
+++ src/Components/ProjectDetails/ProjectDetails.jsx
@@ -1,21 +1,6 @@
 import './projectDetails.css';
 import { useProject } from '../State/ProjectContext';
 
-// const ProjectDetails = ({ project }) => {
-//     if (!project) {
-//         return <div>Loading...</div>
-//     }
-
-//     return (
-//         <div>
-//             <h2>{project.name}</h2>
-//             <p>{project.description}</p>            
-//             {project.images.map((image, index) => (
-//                 <img key={index} src={image} alt={`Image ${index + 1}`} className="projectCard__Img"/>
-//             ))}          
-//         </div>
-//     )
-// }
 
 const ProjectDetails = () => {
     const { project } = useProject();
@@ -28,7 +13,7 @@ const ProjectDetails = () => {
     return (
         <div>
              <h2>{project.name}</h2>
-             <p>{project.description}</p>            
+             <p>{project.description}</p>                        
              {project.images.map((image, index) => (
                  <img key={index} src={image} alt={`Image ${index + 1}`} className="projectCard__Img"/>
              ))}          
