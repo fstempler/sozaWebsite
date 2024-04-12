da607fb80df6cf4741d4e3c9a5eb1f018a6ced93
fstempler <zackmango@gmail.com>
2024-04-12 18:33:55 -0300

    Add Loader to ProjectCard

--- src/Components/Projects/projectCard/ProjectCard.jsx
+++ src/Components/Projects/projectCard/ProjectCard.jsx
@@ -1,13 +1,33 @@
 import './projectCard.css'
+import Loading from '../../Loading/Loading'
+import { useState, useEffect } from 'react';
 
 const  ProjectCard = ({ backgroundImage, title}) => {
     const backgroundImg = {
         backgroundImage: `url(${backgroundImage})`,    
     }
+    const [isLoading, setIsLoading] = useState(true);  
+
+  useEffect(() => {
+    // Simulate asynchronous task
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500); // Simulate 2 seconds of loading
+  }, []);
+
     return (
+        <>
+        {isLoading ? (
+            <div className='loading__Images-Container'>
+              <Loading  />
+            </div>
+            
+          ) : (
         <div className='projectCard__Container' style={backgroundImg}>
             <h2 className='projectCard__Title'>{title}</h2>
         </div>
+          )}
+        </>
     )
 }
 
